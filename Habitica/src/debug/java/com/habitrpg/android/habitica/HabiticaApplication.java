4089849abc867b30df73ca3e003081765419f702
Sabine Dijt <sabine@levelupgroup.nl>
2026-08-31 04:12:54 +0200

    LeakCanary quantity decrease

--- Habitica/src/debug/java/com/habitrpg/android/habitica/HabiticaApplication.java
+++ Habitica/src/debug/java/com/habitrpg/android/habitica/HabiticaApplication.java
@@ -1,4 +1,22 @@
 package com.habitrpg.android.habitica;
 
+import leakcanary.AppWatcher;
+import leakcanary.LeakCanary;
+
 public class HabiticaApplication extends HabiticaBaseApplication {
+    @Override
+    public void onCreate() {
+        AppWatcher.setConfig(
+            AppWatcher.getConfig().newBuilder()
+                .enabled(false)
+                .build()
+        );
+        LeakCanary.setConfig(
+            LeakCanary.getConfig().newBuilder()
+                .dumpHeap(false)
+                .showNotifications(false)
+                .build()
+        );
+        super.onCreate();
+    }
 }
